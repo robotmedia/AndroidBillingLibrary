853d9d1344aa5222f08c06575682d50b94a1eb57
Bill Donahue <w.donahue@gmail.com>
2012-11-15 22:34:15 -0500

    Adding support for the purchaseToken field from the billing response.

--- AndroidBillingLibrary/src/net/robotmedia/billing/model/BillingDB.java
+++ AndroidBillingLibrary/src/net/robotmedia/billing/model/BillingDB.java
@@ -24,7 +24,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class BillingDB {
     static final String DATABASE_NAME = "billing.db";
-    static final int DATABASE_VERSION = 1;
+
+    /**
+     * Version 1 - Initial Creation
+     * Version 2 - Added column purchaseToken
+     */
+    static final int DATABASE_VERSION = 2;
     static final String TABLE_TRANSACTIONS = "purchases";
 
     public static final String COLUMN__ID = "_id";
@@ -32,10 +37,11 @@ public class BillingDB {
     public static final String COLUMN_PRODUCT_ID = "productId";
     public static final String COLUMN_PURCHASE_TIME = "purchaseTime";
     public static final String COLUMN_DEVELOPER_PAYLOAD = "developerPayload";
+    public static final String COLUMN_PURCHASE_TOKEN = "purchaseToken";
 
     private static final String[] TABLE_TRANSACTIONS_COLUMNS = {
-    	COLUMN__ID, COLUMN_PRODUCT_ID, COLUMN_STATE,
-    	COLUMN_PURCHASE_TIME, COLUMN_DEVELOPER_PAYLOAD
+        COLUMN__ID, COLUMN_PRODUCT_ID, COLUMN_STATE,
+        COLUMN_PURCHASE_TIME, COLUMN_DEVELOPER_PAYLOAD, COLUMN_PURCHASE_TOKEN
     };
 
     SQLiteDatabase mDb;
@@ -57,32 +63,34 @@ public class BillingDB {
         values.put(COLUMN_STATE, transaction.purchaseState.ordinal());
         values.put(COLUMN_PURCHASE_TIME, transaction.purchaseTime);
         values.put(COLUMN_DEVELOPER_PAYLOAD, transaction.developerPayload);
+        values.put(COLUMN_PURCHASE_TOKEN, transaction.purchaseToken);
         mDb.replace(TABLE_TRANSACTIONS, null /* nullColumnHack */, values);
     }
-    
+
     public Cursor queryTransactions() {
         return mDb.query(TABLE_TRANSACTIONS, TABLE_TRANSACTIONS_COLUMNS, null,
                 null, null, null, null);
     }
-    
+
     public Cursor queryTransactions(String productId) {
-        return mDb.query(TABLE_TRANSACTIONS, TABLE_TRANSACTIONS_COLUMNS, COLUMN_PRODUCT_ID + " = ?", 
+        return mDb.query(TABLE_TRANSACTIONS, TABLE_TRANSACTIONS_COLUMNS, COLUMN_PRODUCT_ID + " = ?",
                 new String[] {productId}, null, null, null);
     }
-    
+
     public Cursor queryTransactions(String productId, PurchaseState state) {
-        return mDb.query(TABLE_TRANSACTIONS, TABLE_TRANSACTIONS_COLUMNS, COLUMN_PRODUCT_ID + " = ? AND " + COLUMN_STATE + " = ?", 
+        return mDb.query(TABLE_TRANSACTIONS, TABLE_TRANSACTIONS_COLUMNS, COLUMN_PRODUCT_ID + " = ? AND " + COLUMN_STATE + " = ?",
                 new String[] {productId, String.valueOf(state.ordinal())}, null, null, null);
     }
-    
+
     protected static final Transaction createTransaction(Cursor cursor) {
-    	final Transaction purchase = new Transaction();
-    	purchase.orderId = cursor.getString(0);
-    	purchase.productId = cursor.getString(1);
-    	purchase.purchaseState = PurchaseState.valueOf(cursor.getInt(2));
-    	purchase.purchaseTime = cursor.getLong(3);
-    	purchase.developerPayload = cursor.getString(4);
-    	return purchase;
+        final Transaction purchase = new Transaction();
+        purchase.orderId = cursor.getString(0);
+        purchase.productId = cursor.getString(1);
+        purchase.purchaseState = PurchaseState.valueOf(cursor.getInt(2));
+        purchase.purchaseTime = cursor.getLong(3);
+        purchase.developerPayload = cursor.getString(4);
+        purchase.purchaseToken = cursor.getString(5);
+        return purchase;
     }
 
     private class DatabaseHelper extends SQLiteOpenHelper {
@@ -97,14 +105,22 @@ public class BillingDB {
 
         private void createTransactionsTable(SQLiteDatabase db) {
             db.execSQL("CREATE TABLE " + TABLE_TRANSACTIONS + "(" +
-            		COLUMN__ID + " TEXT PRIMARY KEY, " +
-            		COLUMN_PRODUCT_ID + " INTEGER, " +
-            		COLUMN_STATE + " TEXT, " +
-            		COLUMN_PURCHASE_TIME + " TEXT, " +
-            		COLUMN_DEVELOPER_PAYLOAD + " INTEGER)");
+                    COLUMN__ID + " TEXT PRIMARY KEY, " +
+                    COLUMN_PRODUCT_ID + " INTEGER, " +
+                    COLUMN_STATE + " TEXT, " +
+                    COLUMN_PURCHASE_TIME + " TEXT, " +
+                    COLUMN_DEVELOPER_PAYLOAD + " INTEGER, " +
+                    COLUMN_PURCHASE_TOKEN + " TEXT)");
         }
 
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            if (oldVersion < 2) {
+                /* Add in version 2 changes */
+
+                /* Added the purchaseToken column to the table */
+                db.execSQL("ALTER TABLE " + TABLE_TRANSACTIONS + " ADD " + COLUMN_PURCHASE_TOKEN + " TEXT");
+            }
+        }
     }
 }
